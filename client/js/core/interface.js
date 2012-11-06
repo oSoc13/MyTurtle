f52ea936b433617e63f1182e81232c396147dd5f
Jens Segers <segers.jens@gmail.com>
2012-11-06 22:50:41 +0100

    Interface tweaks

--- client/js/core/interface.js
+++ client/js/core/interface.js
@@ -3,28 +3,50 @@
  */
 window.Interface = (function() {
 
+    /*
+     * Execute all functions for passed config
+     */
     function setup(config) {
         color(config.color);
         logo(config.logo);
+        title(config.title);
     }
     
+    /*
+     * Change the ui color
+     */
     function color(value) {
-        var style = "";
+        // style element
+        var custom = $('body style#custom');
+        if (custom.length == 0) {
+            custom = $('<style type="text/css" id="custom"></style>');
+            $('body').prepend(custom);
+        }
         
         // custom styles
-        style += ".text-color { color: " + value + "; }\n";
-        style += ".bg-color { background-color: " + value + "; }\n";
-        style += ".bg-color-light { background-color: " + value + "; }\n";
-        style += ".bg-color-dark { background-color: " + value + "; }\n";
-        style += ".bg-color-alt { background-color: " + value + "; }\n";
-        style += ".border-color { border-color: " + value + " transparent; }\n";
+        var style = ".text-color { color: " + value + "; }\n"
+                  + ".bg-color { background-color: " + value + "; }\n"
+                  + ".bg-color-light { background-color: " + value + "; }\n"
+                  + ".bg-color-dark { background-color: " + value + "; }\n"
+                  + ".bg-color-alt { background-color: " + value + "; }\n"
+                  + ".border-color { border-color: " + value + " transparent; }\n";
         
         // add to body
-        $('body').prepend('<style type="text/css">' + style + '</style>');
+        custom.html(style);
     }
     
+    /*
+     * Set the footer logo url
+     */
     function logo(url) {
         $('footer img#client-logo').attr('src', url);
+    }
+    
+    /*
+     * Set the window title
+     */
+    function title(value) {
+        document.title = value;
     }
 
     /*
@@ -33,7 +55,8 @@ window.Interface = (function() {
     return {
         setup : setup,
         color : color,
-        logo : logo
+        logo : logo,
+        title : title
     };
 
 }());
