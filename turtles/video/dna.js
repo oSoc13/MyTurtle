778c358fa2a2c70661f9df0760b6154129fc75b8
Pieter Colpaert <pieterc@member.fsf.org>
2012-11-18 13:47:37 +0100

    Added shown to video

--- turtles/video/dna.js
+++ turtles/video/dna.js
@@ -7,7 +7,7 @@
 	    $.get('turtles/video/views/widget.html', function(template) {
 		var data = {
 		    location : self.options.location
-		};		
+		};
 		// set window height to load
 		self.$el.height('100%');
 		// render html
@@ -16,16 +16,21 @@
 		self.$el.addClass('nopadding');
                 self.render();
 	    });
+	    this.bind("shown", this.shown);
 	},
 	render : function(){
             //make a difference between the Qt Browser used by FlatTurtle customers and normal browser users
             if(typeof application === 'undefined'){
                 //for normal browsers, let's use HTML5
-                $("#videocanvas").html("<video src='http://localhost/glaverbel.mp4' autoplay=autoplay width=100% height=100%></video>");
+                $("#videocanvas").html("<video src='" + self.options.location + "' autoplay=autoplay width=100% height=100%></video>");
             }else{
                 //application exists, which means we are on a FlatTurtle device. In order to save bandwidth, we will automatically download the video file onto a device an play it using the hardware accelerated phonon qt plugin
-                $("#videocanvas").html("<object></object>");//todo
+                $("#videocanvas").html("<object type=\"application/x-qt-plugin\" classid=\"MyPlayer\" name=\"player\" width=100% height=100%></object>");//todo: set right width and height
+                player.playfile(self.options.location);
             }
+	},
+	shown : function(){
+		//add play function here
 	}
     });
     
