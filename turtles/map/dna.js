f75d702a606db0f408343d795aab5092b5ed870a
Jens Segers <segers.jens@gmail.com>
2012-11-18 12:52:54 +0100

    Turtle tweaks and new shown event

--- turtles/map/dna.js
+++ turtles/map/dna.js
@@ -1,98 +1,103 @@
 (function($){
-	
-	var view = Backbone.View.extend({
-		// hold google maps objects
-		center : null,
-		map : null,
-		traffic : null,
+    
+    var view = Backbone.View.extend({
+        // hold google maps objects
+        center : null,
+        map : null,
+        traffic : null,
 
-		initialize : function() {
-			// default zoom
-			if (!this.options.zoom)
-				this.options.zoom = 13;
-			
-			// get the google maps api
-			$.getScript("http://maps.googleapis.com/maps/api/js?sensor=false&callback=mapsLoaded");
-			
-			// render will be triggered when the google maps api is loaded
-			this.bind("render", this.render);
-			
-			// refresh traffic
-			var self = this; 
-			refreshInterval = window.setInterval(function () {
-				// remove old layer
-				self.traffic.setMap(null);
-				self.traffic = null;
-				
-				// add fresh layer
-				self.traffic = new google.maps.TrafficLayer();
-				self.traffic.setMap(self.map);
-			}, 120000);
-		},
-		render : function() {
-			var self = this;
-			
-			$.get('turtles/map/views/widget.html', function(template) {
-				var data = {
-					location : self.options.location
-				};
-				
-				// set window height to load
-				self.$el.height('100%');
-				
-				// render html
-				self.$el.html(Mustache.render(template, data));
-				
-				// change turtle padding
-				self.$el.addClass('nopadding');
-				
-				// canvas element
-				var canvas = self.$el.find("#canvas")[0];
-				
-				// map options
-				var options = {
-				    zoom : parseInt(self.options.zoom),
-				    disableDefaultUI: true,
-				    mapTypeId : google.maps.MapTypeId.ROADMAP
-				};
-				
-				// create the google map object
-				self.map = new google.maps.Map(canvas, options);
-				
-				// convert location to geocode
-				var geocoder = new google.maps.Geocoder();
-				geocoder.geocode({
-					"address" : self.options.location
-					}, function(results, status) {
-						if (status == google.maps.GeocoderStatus.OK) {
-							self.center = results[0].geometry.location;
-							self.map.setCenter(self.center);
-							
-							var marker = new google.maps.Marker({
-					            map: self.map,
-					            position: results[0].geometry.location
-					        });
-						}
-				});
-				
-				// add traffic layer
-				self.traffic = new google.maps.TrafficLayer();
-				self.traffic.setMap(self.map);
-			});
-		}
-	});
-	
-	// register turtle
-	Turtles.register("map", {
-		view : view
-	});
-	
+        initialize : function() {
+            // prevents loss of "this" inside methods
+            _.bindAll(this, "refresh");
+            
+            // default zoom
+            if (!this.options.zoom)
+                this.options.zoom = 13;
+            
+            // get the google maps api
+            $.getScript("http://maps.googleapis.com/maps/api/js?sensor=false&callback=mapsLoaded");
+            
+            // render will be triggered when the google maps api is loaded
+            this.bind("render", this.render);
+            
+            // refresh traffic
+            refreshInterval = window.setInterval(this.refresh, 120000);
+        },
+        refresh : function() {
+            var self = this; 
+            
+            // remove old layer
+            self.traffic.setMap(null);
+            self.traffic = null;
+            
+            // add fresh layer
+            self.traffic = new google.maps.TrafficLayer();
+            self.traffic.setMap(self.map);
+        },
+        render : function() {
+            var self = this;
+            
+            $.get("turtles/map/views/widget.html", function(template) {
+                var data = {
+                    location : self.options.location
+                };
+                
+                // set window height to load
+                self.$el.height("100%");
+                
+                // render html
+                self.$el.html(Mustache.render(template, data));
+                
+                // change turtle padding
+                self.$el.addClass("nopadding");
+                
+                // canvas element
+                var canvas = self.$el.find("#canvas")[0];
+                
+                // map options
+                var options = {
+                    zoom : parseInt(self.options.zoom),
+                    disableDefaultUI: true,
+                    mapTypeId : google.maps.MapTypeId.ROADMAP
+                };
+                
+                // create the google map object
+                self.map = new google.maps.Map(canvas, options);
+                
+                // convert location to geocode
+                var geocoder = new google.maps.Geocoder();
+                geocoder.geocode({
+                    "address" : self.options.location
+                    }, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            self.center = results[0].geometry.location;
+                            self.map.setCenter(self.center);
+                            
+                            var marker = new google.maps.Marker({
+                                map: self.map,
+                                position: results[0].geometry.location
+                            });
+                        }
+                });
+                
+                // add traffic layer
+                self.traffic = new google.maps.TrafficLayer();
+                self.traffic.setMap(self.map);
+            });
+        }
+    });
+    
+    // register turtle
+    Turtles.register("map", {
+        view : view
+    });
+    
 })(jQuery);
 
 // callback when the google maps api is ready
-if (typeof mapsLoaded != 'function') {
-	function mapsLoaded() {
-		// trigger for all map turtles
-		Turtles.trigger("map", "render");
-	}
+if (typeof mapsLoaded != "function") {
+    function mapsLoaded() {
+        // trigger for all map turtles
+        Turtles.trigger("map", "render");
+    }
 }
--- turtles/nmbs/dna.js
+++ turtles/nmbs/dna.js
@@ -1,160 +1,137 @@
 (function($) {
 
-	var collection = Backbone.Collection.extend({
-		initialize : function(models, options) {
-			// prevents loss of 'this' inside methods
-			_.bindAll(this, "refresh");
-			_.bindAll(this, "parseStation");
+    var collection = Backbone.Collection.extend({
+        initialize : function(models, options) {
+            // prevents loss of "this" inside methods
+            _.bindAll(this, "refresh");
+            _.bindAll(this, "parseStation");
 
-			// bind refresh
-			this.bind("born", this.refresh);
-			this.bind("refresh", this.refresh);
+            // bind refresh
+            this.bind("born", this.refresh);
+            this.bind("refresh", this.refresh);
 
-			// default error value
-			options.error = false;
+            // default error value
+            options.error = false;
 
-			// default distance values
-			options.walking = "00:00";
-			options.bicycling = "00:00";
+            // default distance values
+            options.walking = "00:00";
+            options.bicycling = "00:00";
 
-			// default limit
-			if (!options.limit)
-				options.limit = 5;
+            // default limit
+            if (!options.limit)
+                options.limit = 5;
 
-			// automatic collection refresh each minute, this will
-			// trigger the reset event
-			refreshInterval = window.setInterval(this.refresh, 60000);
-		},
-		refresh : function() {
-			var self = this;
-			self.fetch({
-				error : function() {
-					// will allow the view to detect errors
-					self.options.error = true;
+            // automatic collection refresh each minute, this will
+            // trigger the reset event
+            refreshInterval = window.setInterval(this.refresh, 60000);
+        },
+        refresh : function() {
+            var self = this;
+            self.fetch({
+                error : function() {
+                    // will allow the view to detect errors
+                    self.options.error = true;
 
-					// if there are no previous items to show, display error message
-					if(self.length == 0)
-						self.trigger("reset");
-				}
-			});
-		},
-		url : function() {
-			var self = this;
-			var today = new Date();
-			var month = today.getMonth() + 1;
-			var day = today.getDate();
-			var year = today.getFullYear();
-			var minutes = today.getMinutes();
-			var hours = today.getHours();
+                    // if there are no previous items to show, display error message
+                    if(self.length == 0)
+                        self.trigger("reset");
+                }
+            });
+        },
+        url : function() {
+            var today = new Date();
+            var query = this.options.location + "/" + today.format("{Y}/{m}/{d}/{H}/{M}");
+            
+            // get station name
+            $.getJSON("http://data.irail.be/NMBS/Liveboard/" + query + ".json", this.parseStation);
 
-			if (minutes < 10)
-				minutes = "0" + minutes;
+            return "http://data.irail.be/spectql/NMBS/Liveboard/" + query + "/departures.limit(" + parseInt(this.options.limit) + "):json";
+        },
+        parse : function(json) {
+            // parse ajax results
+            var liveboard = json.spectql;
 
-			if (hours < 10)
-				hours = "0" + hours;
+            for (var i in liveboard) {
+                var time = new Date(liveboard[i].time * 1000);
+                liveboard[i].time = time.format("{H}:{M}");
 
-			if (month < 10)
-				month = "0" + month;
+                if (liveboard[i].delay) {
+                    var delay = new Date(liveboard[i].delay * 1000 + time.getTimezoneOffset() * 60000);
+                    liveboard[i].delay = delay.format("{H}:{M}");
+                }
 
-			if (day < 10)
-				day = "0" + day;
+                if (!liveboard[i].platform.name)
+                    liveboard[i].platform.name = "-";
 
-			var query = this.options.location + "/" + year + "/" + month + "/" + day + "/" + hours + "/" + minutes;
-			
-			$.getJSON("http://data.irail.be/NMBS/Liveboard/" + query + ".json", self.parseStation);
-			// remote source url - todo: add departures or arrivals
-			return "http://data.irail.be/spectql/NMBS/Liveboard/" + query + "/departures.limit(" + parseInt(this.options.limit) + "):json";
-		},
-		parse : function(json) {
-			// parse ajax results
-			var liveboard = json.spectql;
+                liveboard[i].type = liveboard[i].vehicle.match(/\.([a-zA-Z]+)[0-9]+$/)[1];
+                if (!liveboard[i].type)
+                    liveboard[i].type = "-";
+            }
 
-			for (var i in liveboard) {
-				var time = new Date(liveboard[i].time * 1000);
-				liveboard[i].time = this.formatTime(time);
+            return liveboard;
+        },
+        parseStation : function(data) {
+            // get walk and bike times from station location
+            if(this.options.screen_location){
+                var self = this;
+                var fromGeocode = self.options.screen_location;
+                var toGeocode = data.Liveboard.location.latitude + "," + data.Liveboard.location.longitude;
+                
+                Duration.walking(fromGeocode, toGeocode, function(time){
+                    if (self.options.walking != time.format("{H}:{M}")) {
+                        self.options.walking = time.format("{H}:{M}");
+                        self.trigger("reset");
+                    }
+                });
+                Duration.cycling(fromGeocode, toGeocode, function(time){
+                    if (self.options.bicycling != time.format("{H}:{M}")) {
+                        self.options.bicycling = time.format("{H}:{M}");
+                        self.trigger("reset");
+                    }
+                });                                
+            }
+        }
+    });
 
-				if (liveboard[i].delay) {
-					var delay = new Date(liveboard[i].delay * 1000 + time.getTimezoneOffset() * 60000);
-					liveboard[i].delay = this.formatTime(delay);
-				}
+    var view = Backbone.View.extend({
+        initialize : function() {
+            // prevents loss of "this" inside methods
+            _.bindAll(this, "render");
 
-				if (!liveboard[i].platform.name)
-					liveboard[i].platform.name = "-";
+            // bind render to collection reset
+            this.collection.bind("reset", this.render);
 
-				liveboard[i].type = liveboard[i].vehicle.match(/\.([a-zA-Z]+)[0-9]+$/)[1];
-				if (!liveboard[i].type)
-					liveboard[i].type = "-";
-			}
+            // pre-fetch template file and render when ready
+            var self = this;
+            if (this.template == null) {
+                $.get("turtles/nmbs/views/list.html", function(template) {
+                    self.template = template;
+                    self.render();
+                });
+            }
+        },
+        render : function() {
+            // only render when template file is loaded
+            if (this.template) {
+                var data = {
+                    station : this.options.location,
+                    walking : this.options.walking,
+                    bicycling : this.options.bicycling,
+                    entries : this.collection.toJSON(),
+                    color : this.options.color,
+                    error : this.options.error // have there been any errors?
+                };
 
-			return liveboard;
-		},
-		parseStation : function(data) {
-			// get walk and bike times from station location
-			if(this.options.screen_location){
-				var self = this;
-				var fromGeocode = self.options.screen_location;
-				var toGeocode = data.Liveboard.location.latitude + "," + data.Liveboard.location.longitude;
-				
-				Duration.walking(fromGeocode, toGeocode, function(time){
-					if (self.options.walking != self.formatTime(time)) {
-						self.options.walking = self.formatTime(time);
-						self.trigger("reset");
-					}
-				});
-				Duration.cycling(fromGeocode, toGeocode, function(time){
-					if (self.options.bicycling != self.formatTime(time)) {
-						self.options.bicycling = self.formatTime(time);
-						self.trigger("reset");
-					}
-				});								
-			}
-		},
-		formatTime : function(time) {
-			var hours = time.getHours();
-			var minutes = time.getMinutes();
-			return (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes;
-		}
-	});
+                // add html to container
+                this.$el.html(Mustache.render(this.template, data));
+            }
+        }
+    });
 
-	var view = Backbone.View.extend({
-		initialize : function() {
-			// prevents loss of 'this' inside methods
-			_.bindAll(this, "render");
-
-			// bind render to collection reset
-			this.collection.bind("reset", this.render);
-
-			// pre-fetch template file and render when ready
-			var self = this;
-			if (this.template == null) {
-				$.get("turtles/nmbs/views/list.html", function(template) {
-					self.template = template;
-					self.render();
-				});
-			}
-		},
-		render : function() {
-			// only render when template file is loaded
-			if (this.template) {
-				var data = {
-					station : this.options.location,
-					walking : this.options.walking,
-					bicycling : this.options.bicycling,
-					entries : this.collection.toJSON(),
-					color : this.options.color,
-					error : this.options.error // have there been any errors?
-				};
-
-				// add html to container
-				this.$el.html(Mustache.render(this.template, data));
-			}
-		}
-	});
-
-	// register turtle
-	Turtles.register("nmbs", {
-		collection : collection,
-		view : view
-	});
+    // register turtle
+    Turtles.register("nmbs", {
+        collection : collection,
+        view : view
+    });
 
 })(jQuery);
