0d8116d46447149aadded3c3fca1755e6d9c57cb
Michiel Vancoillie <info@ninetynine.be>
2012-11-20 16:31:35 +0100

    Ready for server deployment

--- client/template.php
+++ client/template.php
@@ -1,53 +1,53 @@
 <!DOCTYPE html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>InfoScreen</title>
-        <meta name="description" content="MyTurtle">
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-    
-        <link rel="stylesheet" href="client/css/normalize.css">
-        <link rel="stylesheet" href="client/css/main.css">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
-        <![endif]-->
-			
-        <div id="container" class="clearfix">
-        
-        </div>
-        
-        <footer>
-            <img src="client/css/images/ft_logo.jpg" alt="FlatTurtle" id="flatturtle-logo" />
-            <p id="where" class="text-color">Check in at <span class="bold">location</span> or tag <span class="bold">#location</span></p>
-        	<img src="client/css/images/logo_placeholder.png" alt="Logo" id="client-logo" />
-        </footer>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>InfoScreen</title>
+		<meta name="description" content="MyTurtle">
+		<meta name="viewport" content="width=device-width, user-scalable=no" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
-        <script src="client/js/libs/jquery.js"></script>
-        <script src="client/js/libs/underscore.js"></script>
-        <script src="client/js/libs/backbone.js"></script>
-        <script src="client/js/libs/mustache.js"></script>
-        <script src="client/js/libs/tinycolor.js"></script>
+		<link rel="stylesheet" href="client/css/normalize.css">
+		<link rel="stylesheet" href="client/css/main.css">
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+		<![endif]-->
+
+		<div id="container" class="clearfix">
+
+		</div>
+
+		<footer>
+			<img src="client/css/images/ft_logo.jpg" alt="FlatTurtle" id="flatturtle-logo" />
+			<p id="where" class="text-color">Check in at <span class="bold">location</span> or tag <span class="bold">#location</span></p>
+			<img src="client/css/images/logo_placeholder.png" alt="Logo" id="client-logo" />
+		</footer>
+
+		<script src="client/js/libs/jquery.js"></script>
+		<script src="client/js/libs/underscore.js"></script>
+		<script src="client/js/libs/backbone.js"></script>
+		<script src="client/js/libs/mustache.js"></script>
+		<script src="client/js/libs/tinycolor.js"></script>
 		<script src="client/js/libs/later.js"></script>
-        <script src="client/js/libs/duration.js"></script>
-        
-        <script src="client/js/core/functions.js"></script>
-        <script src="client/js/core/screen.js"></script>
-        <script src="client/js/core/interface.js"></script>
-        <script src="client/js/core/turtles.js"></script>
-        <script src="client/js/core/panes.js"></script>
+		<script src="client/js/libs/duration.js"></script>
+
+		<script src="client/js/core/functions.js"></script>
+		<script src="client/js/core/screen.js"></script>
+		<script src="client/js/core/interface.js"></script>
+		<script src="client/js/core/turtles.js"></script>
+		<script src="client/js/core/panes.js"></script>
 		<script src="client/js/core/jobs.js"></script>
-        
-        <script src="client/js/plugins/clock.js"></script>
-        <script src="client/js/plugins/power.js"></script>
-        <script src="client/js/plugins/message.js"></script>
-        
-        <script>
-            Screen.load('https://s.flatturtle.anothercoolproject.com/hub.json');
-        </script>
-        
-    </body>
+
+		<script src="client/js/plugins/clock.js"></script>
+		<script src="client/js/plugins/power.js"></script>
+		<script src="client/js/plugins/message.js"></script>
+
+		<script>
+			Screen.load('<?php echo $alias; ?>.json');
+		</script>
+
+	</body>
 </html>
--- index.php
+++ index.php
@@ -4,5 +4,26 @@
  * HERE WE GO!
  * --------------------------------------------------------------------
  */
-	
+
+
+define('ENVIRONMENT', 'development');
+
+
+if (defined('ENVIRONMENT'))
+{
+	switch (ENVIRONMENT)
+	{
+		case 'development':
+			$alias = 'https://s.flatturtle.anothercoolproject.com/hub.json';
+		break;
+
+		case 'testing':
+		case 'production':
+			$alias = '/'.$_GET['alias'];
+		break;
+
+		default:
+			exit('The application environment is not set correctly.');
+	}
+}
 require_once 'client/template.php';
