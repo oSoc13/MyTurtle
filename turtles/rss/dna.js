323ac162d649fed91d07c5e3982f9a5f821d535a
Michiel Vancoillie <info@ninetynine.be>
2012-11-21 03:08:58 +0100

    Drop protocol for maps turtle

--- turtles/rss/dna.js
+++ turtles/rss/dna.js
@@ -1,22 +1,22 @@
 (function($) {
-    
+
     var collection = Backbone.Collection.extend({
         initialize : function(models, options) {
             // prevents loss of "this" inside methods
             _.bindAll(this, "refresh");
-            
+
             // fetch data when born
             this.bind("born", this.fetch);
             this.bind("refresh", this.refresh);
-            
+
             // default error value
             options.error = false;
-            
+
             // default limit
             if (!options.limit)
                 options.limit = 5;
-            
-            // automatic collection refresh each minute, this will 
+
+            // automatic collection refresh each minute, this will
             // trigger the reset event
             refreshInterval = window.setInterval(this.refresh, 60000);
         },
@@ -24,13 +24,13 @@
             // don't fetch if there is no feed
             if (this.options.feed == null || !this.options.feed)
                 return;
-            
+
             var self = this;
             self.fetch({
                 error : function(jqXHR, textStatus, errorThrown) {
                     // will allow the view to detect errors
                     self.options.error = true;
-                    
+
                     // if there are no previous items to show, display error message
                     if (self.length == 0)
                         self.trigger("reset");
@@ -39,17 +39,17 @@
         },
         url : function() {
             // remote source url
-            return "http://www.google.com/reader/public/javascript/feed/" + this.options.feed + "?callback=?";
+            return "//www.google.com/reader/public/javascript/feed/" + this.options.feed + "?callback=?";
         },
         parse : function(json) {
             this.options.source = json.title;
             var items = json.items.slice(0, this.options.limit - 1);
-            
+
             for (var i in items) {
                 var time = new Date(items[i].published * 1000);
                 items[i].time = time.format("{H}:{M}");
             }
-            
+
             // return only limited number if items
             return items;
         }
@@ -62,7 +62,7 @@
 
             // bind render to collection reset
             this.collection.bind("reset", this.render);
-            
+
             // pre-fetch template file and render when ready
             var self = this;
             if(this.template == null) {
@@ -80,7 +80,7 @@
                     source : this.options.source,
                     entries : this.collection.toJSON()
                 };
-                
+
                 // add html to container
                 this.$el.html(Mustache.render(this.template, data));
             }
