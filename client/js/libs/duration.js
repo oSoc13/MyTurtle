0cb049eaf532f3bdaa7bf455781e020e5d6208e3
Michiel Vancoillie <info@ninetynine.be>
2012-11-24 00:35:20 +0100

    New duration API link

--- client/js/libs/duration.js
+++ client/js/libs/duration.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * FlatTurtle
  * @author: Michiel Vancoillie
  * @license: AGPLv3
@@ -11,35 +11,35 @@ window.Duration = (function() {
 	function walking(from, to, callback){
 		Duration.calculate('WALK', from, to, callback);
 	}
-	
+
 	/*
 	 * Calculate bike duration for route
 	 */
 	function cycling(from, to, callback){
 		Duration.calculate('BICYCLE', from, to, callback);
 	}
-	
+
 	/*
 	 * Core function
 	 */
 	function calculate(mode, from, to, callback){
 		$.ajax({
-			url: "http://opentripplanner.eu:8080/opentripplanner-api-webapp/ws/plan/?mode=" + mode + "&fromPlace=" + from + "&toPlace=" + to,
+			url: "http://data.irail.be/Geo/Distance/" + from + "/" + to + ".json?mode=" + mode,
 			headers: {
 				'Accept' : 'application/json'
 			},
 			dataType: 'jsonp',
 			success: function(data) {
-				if(data.plan != null && data.plan.itineraries[0].duration){
+				if(data.Distance != null && data.Distance.duration){
 					// Cancel timezone effect
 					var UTC = new Date();
-					var time = new Date(data.plan.itineraries[0].duration + (UTC.getTimezoneOffset()*1000*60));
+					var time = new Date(data.Distance.duration + (UTC.getTimezoneOffset()*1000*60));
 					callback(time);
 				}
 			}
-		});								
+		});
 	}
-	
+
 	/*
      * Public interface to this object
      */
