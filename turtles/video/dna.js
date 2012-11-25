54db2c68c8b27a11c9aaac5cc058c5aa17768848
Pieter Colpaert <pieterc@member.fsf.org>
2012-11-25 19:32:34 +0100

    Chaned video code to replay function on render

--- turtles/video/dna.js
+++ turtles/video/dna.js
@@ -14,20 +14,21 @@
 		self.$el.html(Mustache.render(template, data));
 		// change turtle padding
 		self.$el.addClass('nopadding');
+                //make a difference between the Qt Browser used by FlatTurtle customers and normal browser users
+                if(typeof application === 'undefined'){
+                    //for normal browsers, let's use HTML5
+                    $("#videocanvas").html("<video src='" + self.options.location + "' autoplay=autoplay width=100% height=100%></video>");
+                }else{
+                    //application exists, which means we are on a FlatTurtle device. In order to save bandwidth, we will automatically download the video file onto a device an play it using the hardware accelerated phonon qt plugin
+                    $("#videocanvas").html("<object type=\"application/x-qt-plugin\" classid=\"MyPlayer\" name=\"player\" width=100% height=100%></object>");//todo: set right width and height
+                    player.playfile(self.options.location);
+                }
                 self.render();
 	    });
 	    this.bind("shown", this.shown);
 	},
 	render : function(){
-            //make a difference between the Qt Browser used by FlatTurtle customers and normal browser users
-            if(typeof application === 'undefined'){
-                //for normal browsers, let's use HTML5
-                $("#videocanvas").html("<video src='" + this.options.location + "' autoplay=autoplay width=100% height=100%></video>");
-            }else{
-                //application exists, which means we are on a FlatTurtle device. In order to save bandwidth, we will automatically download the video file onto a device an play it using the hardware accelerated phonon qt plugin
-                $("#videocanvas").html("<object type=\"application/x-qt-plugin\" classid=\"MyPlayer\" name=\"player\" width=100% height=100%></object>");//todo: set right width and height
-                player.playfile(self.options.location);
-            }
+            player.replay();
 	},
 	shown : function(){
 		//add play function here
