64c616fb8f364d8cfc41869a358c904abb1e5a8e
Michiel Vancoillie <info@ninetynine.be>
2012-11-27 12:30:12 +0100

    Google analytics default enable

--- client/js/core/functions.js
+++ client/js/core/functions.js
@@ -1,13 +1,13 @@
-/* 
+/*
  * FlatTurtle
  * Helper functions
- * 
+ *
  * @author: Jens Segers (jens@irail.be)
  * @license: AGPLv3
  */
 
  /*
-  * Set e default timeout of 15 seconds for all ajax requests
+  * Set a default timeout of 15 seconds for all ajax requests
   */
 $.ajaxSetup({
 	timeout: 15000
@@ -19,10 +19,10 @@ $.ajaxSetup({
 function sort(items, attribute) {
     if (attribute == null)
         attribute = 'data-order';
-    
+
     var parent = items.parent();
     var list = items.get();
-    
+
     // sort by data-order attribute
     list.sort(function(a, b) {
         // both are plain javascript objects now
@@ -30,7 +30,7 @@ function sort(items, attribute) {
             return 0;
         return a.getAttribute(attribute) < b.getAttribute(attribute) ? -1 : 1;
     });
-    
+
     $.each(list, function(idx, itm) { parent.append(itm); });
 }
 
@@ -47,7 +47,7 @@ var Debug = {
 
 /*
  * A friendly time format function (unix style)
- * 
+ *
  * {Y} - 4 digit year
  * {m} - month with leading zero
  * {d} - day with leading zero
@@ -58,43 +58,43 @@ var Debug = {
 Date.prototype.format = function(format) {
 
     var date = this;
-    
+
     // 4 digit year
     format = format.replace('{Y}', function() {
         return date.getFullYear();
     });
-    
+
     // month with leading zero
     format = format.replace('{m}', function() {
         var month = date.getMonth() + 1;
         return (month < 10 ? "0" : "") + month;
     });
-    
+
     // day with leading zero
     format = format.replace('{d}', function() {
         var day = date.getDate();
         return (day < 10 ? "0" : "") + day;
     });
-    
+
     // hours with leading zero
     format = format.replace('{H}', function() {
         var hours = date.getHours();
         return (hours < 10 ? "0" : "") + hours;
     });
-    
+
     // minutes with leading zero
     format = format.replace('{M}', function() {
         var minutes = date.getMinutes();
         return (minutes < 10 ? "0" : "") + minutes;
     });
-    
+
     // seconds with leading zero
     format = format.replace('{S}', function() {
         var seconds = date.getSeconds();
         return (seconds < 10 ? "0" : "") + seconds;
     });
-    
-    return format;    
+
+    return format;
 }
 
 /*
