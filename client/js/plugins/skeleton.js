93b3bf8e79205be972e78c8dfe89dfa7b86e9908
Jens Segers <segers.jens@gmail.com>
2012-12-01 12:45:56 +0100

    Plugin skeleton

---
+++ client/js/plugins/skeleton.js
@@ -0,0 +1,21 @@
+/* 
+ * FlatTurtle
+ * Example plugin skeleton
+ * 
+ * @author: Jens Segers (jens@irail.be)
+ * @license: AGPLv3
+ */
+
+var Skeleton = {
+
+    /*
+     * Enable this plugin
+     */
+	enable : function() {},
+
+	/*
+     * Disable this plugin
+     */
+	disable : function() {}
+
+};
