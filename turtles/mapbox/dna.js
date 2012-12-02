22d83039eac05f9893cc30ea850964b3705a0aa8
Jens Segers <segers.jens@gmail.com>
2012-12-03 00:03:35 +0100

    Mapbox turtle, but something fishy with coordinates

---
+++ turtles/mapbox/dna.js
@@ -0,0 +1,69 @@
+/*
+ * FlatTurtle
+ * @author: Jens Segers (jens@irail.be)
+ * @license: AGPLv3
+ */
+
+(function($){
+
+    var view = Backbone.View.extend({
+		// mapbox object
+		map : null,
+
+		initialize : function() {
+		    var self = this;
+		    
+			// default zoom
+			if (!this.options.zoom)
+				this.options.zoom = 13;
+
+            // for some reason mapbox breaks mustache, so save a backup
+            var Mustache = window.Mustache;
+            
+			// get the google maps api
+			$.getScript("http://api.tiles.mapbox.com/mapbox.js/v0.6.6/mapbox.js", function() {
+			    // reset backup
+			    window.Mustache = Mustache;
+			    
+			    // render map
+	            self.render();
+			});
+		},
+		render : function() {
+			var self = this;
+
+			$.get("turtles/mapbox/views/widget.html", function(template) {
+				var data = {
+					location : Screen.location.address
+				};
+
+				// set window height to load
+				self.$el.height("100%");
+				
+				// render html
+				self.$el.empty();
+				self.$el.html(window.Mustache.render(template, data));
+
+				// change turtle padding
+				self.$el.addClass("nopadding");
+
+				// canvas element
+				var canvas = self.$el.find("#canvas")[0];
+
+				// create the mapbox object
+				self.map = mapbox.map(canvas);
+				self.map.addLayer(mapbox.layer().id('examples.map-vyofok3q'));
+				
+				// center and zoom
+				self.map.center({ lat: Screen.location.latitude, lon: Screen.location.longitude });
+				self.map.zoom(parseInt(self.options.zoom));
+			});
+		}
+    });
+
+	// register turtle
+	Turtles.register("mapbox", {
+		view : view
+	});
+
+})(jQuery);
