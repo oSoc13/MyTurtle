2e0172201571c212f60b6e6c66a1fa31f2ed3d5a
Michiel Vancoillie <info@ninetynine.be>
2012-12-02 21:45:05 +0100

    Logic away from view

--- turtles/video/dna.js
+++ turtles/video/dna.js
@@ -1,43 +1,47 @@
 (function($){
-    
-    var view = Backbone.View.extend({
 
-	initialize : function() {
-            var self = this; 
-	    $.get('turtles/video/views/widget.html', function(template) {
-		var data = {
-		    location : self.options.location
-		};
+	var view = Backbone.View.extend({
+
+		initialize : function() {
+			var self = this;
+			$.get('turtles/video/views/widget.html', function(template) {
+				var data = {
+					location : self.options.location
+				};
 		// set window height to load
 		self.$el.height('100%');
 		// render html
 		self.$el.html(Mustache.render(template, data));
 		// change turtle padding
 		self.$el.addClass('nopadding');
-                //make a difference between the Qt Browser used by FlatTurtle customers and normal browser users
-                if(typeof application === 'undefined'){
-                    //for normal browsers, let's use HTML5
-                    $("#videocanvas").html("<video id='videotag' src='https://my.flatturtle.com/uploads/hub/" + self.options.location + "' width=100% height=100%></video>"); //TODO: replace hub with alias name
-                }
-                self.render();
-	    });
-	    this.bind("shown", this.shown);
-	},
-	render : function(){
-	},
-        shown : function(){
-            if(typeof application !== 'undefined'){
-                $("#playerobject").get(0).replay();
-            }else{
-                $("#videotag").get(0).play();
-            }
-	}
-    });
-    
-    // register turtle
-    Turtles.register("video", {
-	view : view
-    });
-    
+				//make a difference between the Qt Browser used by FlatTurtle customers and normal browser users
+				if(typeof application === 'undefined'){
+					//for normal browsers, let's use HTML5
+					$("#videocanvas").html("<video id='videotag' src='https://my.flatturtle.com/uploads/hub/" + self.options.location + "' width=100% height=100%></video>"); //TODO: replace hub with alias name
+				}
+				self.render();
+			});
+			this.bind("shown", this.shown);
+		},
+		render : function(){
+				//play the movie
+				$("#playerobject").get(0).playfile("/home/flatturtle/" + this.options.location);
+			},
+			shown : function(){
+				if(typeof application !== 'undefined'){
+				//make sure music is turned off
+				application.soundControl("stop");
+				$("#playerobject").get(0).replay();
+			}else{
+				$("#videotag").get(0).play();
+			}
+		}
+	});
+
+	// register turtle
+	Turtles.register("video", {
+		view : view
+	});
+
 })(jQuery);
 
