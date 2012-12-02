5773b47b243ddce7da4f7c631b34ba7a974852ee
Michiel Vancoillie <info@ninetynine.be>
2012-12-02 22:05:21 +0100

    Video turtle height

--- turtles/video/dna.js
+++ turtles/video/dna.js
@@ -10,31 +10,29 @@
 				};
 				// set window height to load
 				self.$el.height('100%');
+
 				// render html
 				self.$el.html(Mustache.render(template, data));
-				// change turtle padding
-				self.$el.addClass('nopadding');
+
 				//make a difference between the Qt Browser used by FlatTurtle customers and normal browser users
 				if(typeof application === 'undefined'){
 					//for normal browsers, let's use HTML5
 					$("#videocanvas").html("<video id='videotag' src='https://my.flatturtle.com/uploads/hub/" + self.options.location + "' width=100% height=100%></video>"); //TODO: replace hub with alias name
 				}
+
 				self.render();
 			});
 			this.bind("shown", this.shown);
 		},
 		render : function(){
-			if(typeof application !== 'undefined'){
+			// change turtle padding
+			this.$el.addClass('nopadding');
+		},
+		shown : function(){
+				if(typeof application !== 'undefined'){
 					//make sure music is turned off
 					application.soundControl("stop");
-					//play the movie
 					$("#playerobject").get(0).playfile("/home/flatturtle/" + this.options.location);
-				}
-			},
-			shown : function(){
-				if(typeof application !== 'undefined'){
-					$("#playerobject").get(0).playfile("/home/flatturtle/" + this.options.location);
-				// $("#playerobject").get(0).replay();
 			}else{
 				$("#videotag").get(0).play();
 			}
