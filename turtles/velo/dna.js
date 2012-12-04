0f6fa5dd5eeec0b59717182bcc090b639053cd8e
Jens Segers <segers.jens@gmail.com>
2012-12-04 22:52:58 +0100

    Velo turtle

---
+++ turtles/velo/dna.js
@@ -0,0 +1,116 @@
+(function($) {
+    
+    var collection = Backbone.Collection.extend({
+        initialize : function(models, options) {
+            // prevents loss of 'this' inside methods
+            _.bindAll(this, "refresh", "configure");
+
+            // bind events
+            this.on("born", this.configure);
+            this.on("born", this.refresh);
+            this.on("refresh", this.refresh);
+            this.on("reconfigure", this.configure);
+
+            // default error value
+            options.error = false;
+            
+            // default distance values
+            options.walking = "00:00";
+
+            // automatic collection refresh each minute, this will 
+            // trigger the reset event
+            refreshInterval = window.setInterval(this.refresh, 60000);
+        },
+        configure : function() {
+            // don't fetch if there is no location
+            if (this.options.location == null || !this.options.location)
+                return;
+            
+            var velo = this.get(0);
+            if (velo != undefined && Screen.location) {
+                var fromGeocode = Screen.location.geocode;
+                var toGeocode = velo.latitude + "," + velo.longitude;
+                
+                Duration.walking(fromGeocode, toGeocode, function(time){
+                    if (self.options.walking != time.format("{H}:{M}")) {
+                        self.options.walking = time.format("{H}:{M}");
+                        self.trigger("reset");
+                    }
+                });                               
+            }
+        },
+        refresh : function() {
+            // don't fetch if there is no location
+            if (this.options.location == null || !this.options.location)
+                return;
+            
+            var self = this;
+            self.fetch({
+                error : function() {
+                    // will allow the view to detect errors
+                    self.options.error = true;
+
+                    // if there are no previous items to show, display error message
+                    if(self.length == 0)
+                        self.trigger("reset");
+                }
+            });
+        },
+        url : function() {
+            var latitude = this.options.location.split(',')[0];
+            var longitude = this.options.location.split(',')[1];
+            
+            return "http://data.irail.be/Bikes/Velo.json?lat=" + encodeURIComponent(latitude) + "&long=" + encodeURIComponent(longitude) + "&offset=0&rowcount=1";
+        },
+        parse : function(json) {
+            var velo = json.Velo;
+            
+            for(var i in velo) {
+                velo[i].distance = Math.round(parseInt(velo[i].distance)/10)*10;
+                velo[i].name = jQuery.trim(velo[i].name).capitalize();
+            }
+            
+            return velo;
+        }
+    });
+
+    var view = Backbone.View.extend({
+        initialize : function() {
+            // prevents loss of 'this' inside methods
+            _.bindAll(this, "render");
+            
+            // bind render to collection reset
+            this.collection.on("reset", this.render);
+
+            // pre-fetch template file and render when ready
+            var self = this;
+            if (this.template == null) {
+                $.get("turtles/velo/views/list.html", function(template) {
+                    self.template = template;
+                    self.render();
+                });
+            }
+        },
+        render : function() {
+            // only render when template file is loaded
+            if (this.template && this.collection.length) {
+            
+                var data = this.collection.toJSON()[0];
+                data.freespots += data.freebikes;
+                data.walking =  this.options.walking;
+                data.error = this.options.error; // have there been any errors?
+                
+                // add html to container
+                this.$el.empty();
+                this.$el.html(Mustache.render(this.template, data));
+            }
+        }
+    });
+
+    // register turtle
+    Turtles.register("velo", {
+        collection : collection,
+        view : view
+    });
+
+})(jQuery);
