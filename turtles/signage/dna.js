f18a655619fe7dd9152aa7452b3451d4377af598
Michiel Vancoillie <info@ninetynine.be>
2012-12-11 03:02:24 +0100

    Make signage turtle dynamic

--- turtles/signage/dna.js
+++ turtles/signage/dna.js
@@ -4,26 +4,37 @@
  * @license: AGPLv3
  */
 
-(function($){
+ (function($){
 
-    var view = Backbone.View.extend({
+	var collection = Backbone.Collection.extend({
+		initialize : function(models, options) {
+			options.data = JSON.parse(options.data);
+		}
+	});
+
+	var view = Backbone.View.extend({
 
 		initialize : function() {
-		    this.on("born", this.render);
+			this.on("born", this.render);
 		},
 		render : function() {
 			var self = this;
 
 			$.get("turtles/signage/views/widget.html", function(template) {
+				var data = {
+					data : self.options.data
+				};
+
 				// render html
 				self.$el.empty();
-				self.$el.html(Mustache.render(template));
+				self.$el.html(Mustache.render(template, data));
 			});
 		}
-    });
+	});
 
 	// register turtle
 	Turtles.register("signage", {
+		collection : collection,
 		view : view
 	});
 
