73bbc4f519980870bb272d866b9f2e75cd8f2437
Michiel Vancoillie <info@ninetynine.be>
2012-12-18 12:11:23 +0100

    Proxy update

--- client/js/libs/mapbox.php
+++ client/js/libs/mapbox.php
@@ -1,7 +1,7 @@
 <?php
 $ch = curl_init();
 $timeout = 5;
-curl_setopt($ch, CURLOPT_URL, "http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js");
+curl_setopt($ch, CURLOPT_URL, "https://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -9,5 +9,6 @@ $data = curl_exec($ch);
 curl_close($ch);
 
 Header("content-type: application/x-javascript");
+$data = preg_replace('/http:\/\//', 'https://', $data);
 echo $data;
 ?>
