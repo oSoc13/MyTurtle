75edeed8202ee4be60bddc6048586456de398648
Michiel Vancoillie <info@ninetynine.be>
2013-04-20 12:08:58 +0200

    Update analytics tracking code #97

--- client/js/plugins/google.js
+++ client/js/plugins/google.js
@@ -13,15 +13,14 @@ var Google = {
 
     enable : function(account, domain) {
         log.info("PLUGIN - GOOGLE - Enable (account: " + account + ", domain: " + domain + ")");
-        _gaq.push(['_setAccount', account]);
-        _gaq.push(['_setDomainName', domain]);
-        _gaq.push(['_trackPageview']);
 
-        (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-        })();
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', account, domain);
+        ga('send', 'pageview');
     },
 
     disable : function() {
@@ -34,4 +33,4 @@ var Google = {
 };
 
 // Default behaviour
-Google.enable('UA-31282630-1', 'flatturtle.com');
+Google.enable('UA-31282630-3', 'flatturtle.com');
